3945c5e3d0a7252d165481fe275922a902704be3
Wachid Amirul M <wachidamirul@gmail.com>
2025-12-20 12:47:59 +0700

    feat: send clean text to llm and print result from llm

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from core.summarizer import summarize
 from utils.pdf_loader import extract_text
 from utils.text_cleaner import clean_text
 
 if __name__ == '__main__':
     text = clean_text(extract_text("data/pdf/temp.pdf"))
 
+    pdf_summarize = summarize(text)
 
-    print(f"{text}")
+    print(f"{pdf_summarize}")
